b5b4800091f2b94eb326b094cde7af41a2bcb781
ayomoses <odukoyaayodeji@gmail.com>
2024-01-10 15:06:59 +0000

    social icons about me section and animation with mobile and desktop

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+
+import SVG from 'react-inlinesvg';
+
+import linkedin from '../assets/icons/linkedin-colored.svg';
+import twitter from '../assets/icons/twitterx.svg';
+import dribbble from '../assets/icons/dribbble.svg';
+import github from '../assets/icons/github-white.svg';
+import { Link } from 'react-router-dom';
+
+export const AboutMeSection = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleHover = () => {
+    setIsHovered(true);
+    console.log('i have entered');
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    console.log('i have left');
+  };
+  return (
+    <section className="about-me">
+      <div
+        className={`about-me__col-one ${isHovered ? 'pause-animation' : ''}`}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div>
+          <h4 className="info mobile-info">
+            click any part of your device to stop objects from moving
+          </h4>
+          <h4 className="info desktop-info">
+            Hover on the icons to check me on socials!
+          </h4>
+        </div>
+
+        <div className='about-me__icons-wrap'>
+          <div className="ball">
+            <Link
+              to="https://www.linkedin.com/in/ayodejiodukoya/"
+              target="_blank"
+            >
+              <SVG src={linkedin} />
+            </Link>
+          </div>
+          <div className="ballA">
+            <Link to="https://twitter.com/ayodeji_odu" target="_blank">
+              <SVG src={twitter} />
+            </Link>
+          </div>
+          <div className="ballB">
+            <Link to="https://twitter.com/ayodeji_odu" target="_blank">
+              <SVG src={dribbble} />
+            </Link>
+          </div>
+          <div className="ballC">
+            <Link to="https://github.com/ayoMoses" target="_blank">
+              <SVG src={github} />
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="about-me__col-two">
+        <p className="bold p-one">
+          I am a Frontend UI Developer and experience as a UI Designer. I am
+          Passionate about creativity and creating dynamic user experience and
+          user interface.
+        </p>
+
+        <p>
+          Oh, Ayo loves motion CSS. My role is to empathize with developers
+          while working on iteration with speed to hasten delivery, features
+          that are feasible from the design end to implement in front-end. I
+          bridge the design and front-end gap during product development and
+          launch.
+        </p>
+
+        <p>
+          I'm a huge fan of anything tech that interests me and that explains my
+          other fields which are: Digital Marketing, Web Analytics and Social
+          Media Strategy.
+        </p>
+
+        <p>
+          I work with Html, CSS, SCSS, Bootstrap, Tailwind CSS, and JavaScript.
+          I take up wordpress projects if required by clients as previous
+          projects on wordpress are shown in my selected projects section.
+        </p>
+      </div>
+    </section>
+  );
+};
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,9 +1,11 @@
+import { AboutMeSection } from '../../components/AboutMe';
 import { HeroSection } from '../../components/HeroSection';
 
 export const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <AboutMeSection />
     </>
   );
 };
