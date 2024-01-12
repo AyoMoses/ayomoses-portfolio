467f2a107344a3c2bbecb8ad86ab22b2c84d12f2
ayomoses <odukoyaayodeji@gmail.com>
2024-01-12 19:44:40 +0000

    featured projects section

--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -1,25 +1,19 @@
 import { useState } from 'react';
 import { MockData } from '../mock/HomeProjects';
 
-// interface Project {
-//   imageUrl: string;
-//   videoUrl: string;
-//   techStack: string[];
-//   category: string;
-//   projectName: string;
-//   description: string;
-//   projectUrl: string;
-// }
+interface HoverState {
+  [key: number]: boolean;
+}
 
 export const FeaturedProjects = () => {
-  const [hover, setHover] = useState(false);
+  const [hover, setHover] = useState<HoverState>({});
   const featuredData = MockData[0]?.featuredData || [];
 
-  const handleMouseEnter = () => {
-    setHover(true);
+  const handleMouseEnter = (index: number) => {
+    setHover((prev) => ({ ...prev, [index]: true }));
   };
-  const handleMouseLeave = () => {
-    setHover(false);
+  const handleMouseLeave = (index: number) => {
+    setHover((prev) => ({ ...prev, [index]: false }));
   };
 
   return (
@@ -29,33 +23,49 @@ export const FeaturedProjects = () => {
         <div
           key={index}
           className="featured__section"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
+          onMouseEnter={() => handleMouseEnter(index)}
+          onMouseLeave={() => handleMouseLeave(index)}
         >
-          {hover && project.videoUrl.length > 1 ? (
-            <video
-              src={project.videoUrl}
-              autoPlay
-              muted
-              loop
-              className="featured__video"
-            />
-          ) : (
-            <img
-              src={project.imageUrl}
-              alt={project.projectName}
-              className="featured__image"
-            />
-          )}
+          <div className="featured__section-inner">
+            <div
+              className={`featured__media-wrap ${
+                hover[index] && project.videoUrl ? 'show-video' : 'hide-video'
+              }`}
+            >
+              {hover[index] && project.videoUrl ? (
+                <video
+                  src={project.videoUrl}
+                  autoPlay
+                  muted
+                  loop
+                  className="featured__video"
+                />
+              ) : (
+                <img
+                  src={project.imageUrl}
+                  alt={project.projectName}
+                  className="featured__image"
+                />
+              )}
+            </div>
 
-          <h5 className="featured__project-name">{project.projectName}</h5>
-          <div className="featured__tags">
-            <ul className="featured__stack">
-              <li className="featured__stack-list">{project.techStack}</li>
-            </ul>
-            <p className="featured__category">{project.category}</p>
+            <div className="featured__tags-wrap">
+              <div className="featured__tags">
+                <ul className="featured__stack">
+                  {project.techStack.map((stack, index, array) => (
+                    <li className="featured__stack-list" key={index}>
+                      {stack}
+                      {index < array.length - 1 && ','}
+                    </li>
+                  ))}
+                </ul>
+                <span>//</span>
+                <p className="featured__category">{project.category}</p>
+              </div>
+              <h5 className="featured__project-name">{project.projectName}</h5>
+            </div>
+            <p className="featured__description">{project.description}</p>
           </div>
-          <p className="featured__description">{project.description}</p>
         </div>
       ))}
     </div>
