a72d8aa2f19654469eb4d80d6b763a115661233a
ayomoses <odukoyaayodeji@gmail.com>
2024-01-31 15:13:20 +0000

    project link added

--- src/pages/Projects/ProjectSlider.tsx
+++ src/pages/Projects/ProjectSlider.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import { MockData } from '../../mock/HomeProjects';
+import { Link } from 'react-router-dom';
+import SVG from 'react-inlinesvg';
+
+import rightIcon from '../../assets/icons/white-right-icon.svg';
+
+// import ReactSlick from 'react-slick';
+
+// import type { Settings } from 'react-slick';
+
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
 
 interface ProjectSliderProps {
   projStack: string;
@@ -38,6 +49,22 @@ export const ProjectSlider: React.FC<ProjectSliderProps> = ({
               </div>
               <h5 className="projects__project-name">{project.projectName}</h5>
               <p className="projects__description">{project.description}</p>
+
+              {/* <Link
+                to={`${project.projectUrl}`}
+                target="_blank"
+                className="button button--black button--black-desktop"
+              >
+                <span>view project</span>
+              </Link> */}
+              <Link
+                to={`${project.projectUrl}`}
+                target="_blank"
+                className="button button--black button--black-mobile"
+              >
+                View project
+                <SVG src={rightIcon} />
+              </Link>
             </div>
           </div>
 
