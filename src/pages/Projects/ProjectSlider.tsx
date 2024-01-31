c5633a7e4627017d6be4a2264675fd4c0835b54b
ayomoses <odukoyaayodeji@gmail.com>
2024-01-31 15:06:51 +0000

    refactor code on projects slider

--- src/pages/Projects/ProjectSlider.tsx
+++ src/pages/Projects/ProjectSlider.tsx
@@ -1,46 +1,64 @@
+import React from 'react';
 import { MockData } from '../../mock/HomeProjects';
 
 interface ProjectSliderProps {
   projStack: string;
+  sectionHeader: string;
 }
 
-export const ProjectSlider = ({ projStack }: ProjectSliderProps) => {
+export const ProjectSlider: React.FC<ProjectSliderProps> = ({
+  projStack,
+  sectionHeader,
+}) => {
   const ProjData = MockData[0]?.projectsData || [];
-  return ProjData.filter((data) => data.techStack.includes(projStack)).map(
-    (project, index) => (
-      <div key={index} className="projects__inner">
-        <div>
-          <div className="projects__tags-wrap">
-            <div className="projects__tags">
-              <ul className="projects__stack">
-                {project.techStack.map((stack, index, array) => (
-                  <li className="projects__stack-list" key={index}>
-                    {stack}
-                    {index < array.length - 1 && ','}
-                  </li>
-                ))}
-              </ul>
-              <span>//</span>
-              <p className="projects__category">{project.category}</p>
+
+  const filteredProjects = ProjData.filter((data) =>
+    data.techStack.includes(projStack)
+  );
+
+  return (
+    <section className="projects__section">
+      <h2 className="projects__section-header">{sectionHeader}</h2>
+
+      {filteredProjects.map((project, index) => (
+        <div key={index} className="projects__inner">
+          <div>
+            <div className="projects__tags-wrap">
+              <div className="projects__tags">
+                <ul className="projects__stack">
+                  {project.techStack.map((stack, index, array) => (
+                    <li className="projects__stack-list" key={index}>
+                      {stack}
+                      {index < array.length - 1 && ','}
+                    </li>
+                  ))}
+                </ul>
+                <span>//</span>
+                <p className="projects__category">{project.category}</p>
+              </div>
+              <h5 className="projects__project-name">{project.projectName}</h5>
+              <p className="projects__description">{project.description}</p>
             </div>
-            <h5 className="projects__project-name">{project.projectName}</h5>
-            <p className="projects__description">{project.description}</p>
+          </div>
+
+          <div className="projects__image-container">
+            {project.mobileimageUrl && (
+              <img
+                src={project.mobileimageUrl}
+                alt={project.projectName}
+                className="featured__image featured__image-mobile"
+              />
+            )}
+            {project.desktopimageUrl && (
+              <img
+                src={project.desktopimageUrl}
+                alt={project.projectName}
+                className="featured__image featured__image-desktop"
+              />
+            )}
           </div>
         </div>
-
-        <div className='projects__image-container'>
-          <img
-            src={project.mobileimageUrl}
-            alt={project.projectName}
-            className="featured__image featured__image-mobile"
-          />
-          <img
-            src={project.desktopimageUrl}
-            alt={project.projectName}
-            className="featured__image featured__image-desktop"
-          />
-        </div>
-      </div>
-    )
+      ))}
+    </section>
   );
 };
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -5,11 +5,10 @@ export const ProjectsPage = () => {
     <div className="projects">
       <h1 className="projects__header">Projects</h1>
 
-      <section className="projects__section">
-        <h2 className="projects__section-header">ReactJS Projects</h2>
-
-        <ProjectSlider projStack="ReactJS" />
-      </section>
+      <ProjectSlider
+        projStack="ReactJS"
+        sectionHeader="ReactJS based Projects"
+      />
     </div>
   );
 };
